d52a3cd583082309eac3695fc997eae2b0d05d68
Perttu Lähteenlahti <perttu@lahteenlahti.com>
2022-01-22 22:12:45 +0200

    NotificationViewModel

--- SwiftUICombine/SwiftUICombine/Models/NotificationViewModel.swift
+++ SwiftUICombine/SwiftUICombine/Models/NotificationViewModel.swift
@@ -13,5 +13,24 @@ import FirebaseMessaging
 
 class NotificationViewModel: ObservableObject {
     @Published var permission: UNAuthorizationStatus?
-
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings() { permission in
+            DispatchQueue.main.async {
+                self.permission = permission.authorizationStatus
+            }
+            
+            if(permission.authorizationStatus == .authorized) {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            } else {
+                DispatchQueue.main.async {
+                    UIApplication.shared.unregisterForRemoteNotifications()
+                    UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+                }
+            }
+            
+        }
+    }
 }
